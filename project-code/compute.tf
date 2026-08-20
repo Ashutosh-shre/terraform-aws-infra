7bd6d2ce0f696a413d831d62f9267fdd910f9010
ashutosh-shre <ashutoshkumar533@gmail.com>
2026-08-20 18:14:03 +0530

    fixing code issue

--- project-code/compute.tf
+++ project-code/compute.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "main" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.main.key_name
-  vpc_security_group_ids =  [aws_security_group.main.id]
+  vpc_security_group_ids      = [aws_security_group.main.id]
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
   root_block_device {
